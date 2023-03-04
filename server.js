100f1df770c94c5659161ab2e256cd172924dfcf
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-03-04 17:40:53 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 // dependencies
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
 
 //app
-const app = express()
+const app = express();
 
 //configure settings
-require('dotenv').config()
-const PORT = process.env.PORT
+require('dotenv').config();
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 
 //connection to mongodb
+mongoose.set('strictQuery', false);
+mongoose.connect(DATABASE_URL);
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Connected to MongoDb')
+})
+db.on('error', (error) => {
+    console.log('Error occured with MongoDb' + error.message)
+});
+
+
 
 
 //middleware
@@ -21,10 +35,10 @@ app.get('/', (req, res) => {
     res.json({
         response: 'hello'
     })
-})
+});
 
 
 // app listen
 app.listen(PORT, () => {
     console.log(`Express is listening on port: ${PORT}`)
-})
+});
